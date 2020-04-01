7a44203e430ed00a0678b992a5cfeae1465c0c4c
Savitar97 <atoth1571@gmail.com>
2020-04-01 19:08:18 +0200

    beadando error year

---
+++ src/main/java/LegoSet/Main.java
@@ -0,0 +1,42 @@
+package LegoSet;
+
+import jaxb.JAXBHelper;
+
+import javax.xml.bind.JAXBException;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) throws JAXBException, FileNotFoundException {
+        LegoSet legoSet=new LegoSet();
+        legoSet.setNumber(75211);
+        legoSet.setName("Imperial TIE Fighter");
+        legoSet.setTheme("Star Wars");
+        legoSet.setSubtheme("Solo");
+        legoSet.setYear(Year.of(2018));
+        legoSet.setPieces(519);
+
+        ArrayList<Minifig> minifigs=new ArrayList<>();
+        minifigs.add(new Minifig(2,"Imperial Mudtrooper"));
+        minifigs.add(new Minifig(1,"Imperial Pilot"));
+        minifigs.add(new Minifig(1,"Mimban Stormtrooper"));
+        legoSet.setMinifigs(minifigs);
+        Set<String> tags = new HashSet<>();
+        tags.add("Starfighter");
+        tags.add("Stormtrooper");
+        tags.add("Star Wars");
+        tags.add("Solo");
+        legoSet.setTags(tags);
+        legoSet.setWeight(new Weight(0.89,"kg"));
+
+        legoSet.setUrl("https://brickset.com/sets/75211-1/Imperial-TIE-Fighter");
+        JAXBHelper.toXML(legoSet, System.out);
+
+        JAXBHelper.toXML(legoSet, new FileOutputStream("lego.xml"));
+
+    }
+}
